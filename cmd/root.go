e0bbc5a4963868e373c80b27212e8351215f12d9
Wout Van De Wiel <wout.vandewiel@accelleran.com>
2024-06-16 13:20:04 +0200

    docs: extend the long description

--- cmd/root.go
+++ cmd/root.go
@@ -30,7 +30,14 @@ var rootCmd = &cobra.Command{
 	Use:   "arc-cleaner",
 	Short: "A cleaner for GitHub ARC",
 	Long: `ARC cleaner is an application to clean up resources from the GitHub
-Actions Runner Controller (ARC).`,
+Actions Runner Controller (ARC).
+
+GitHub Actions Runners in kubernetes mode sometimes get stuck as ephemeral
+volumes are used. These are tied to the lifetime of the runner pod.
+When a runner pod exits or crashes while a workflow pod is still running,
+then the runner gets stuck waiting indefinitely for storage.
+By cleaning up the workflow pod and thus detaching the volume,
+the runner can become available again.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 		logger := slog.Default()
